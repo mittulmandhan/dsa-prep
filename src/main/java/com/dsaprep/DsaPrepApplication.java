25eb0e6cef8fac9b505477e5b861d049515d0f98
tarunmandhan <mandhantarun@gmail.com>
2021-02-18 01:28:37 +0530

    gradle project added

---
+++ src/main/java/com/dsaprep/DsaPrepApplication.java
@@ -0,0 +1,13 @@
+package com.dsaprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaPrepApplication.class, args);
+	}
+
+}
